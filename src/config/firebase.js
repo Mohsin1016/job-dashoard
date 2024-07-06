c3bc9020f6c1470e721c9e83564f1115695e1cef
asadkhalid566 <76807556+asadkhalid566@users.noreply.github.com>
2024-07-06 18:23:25 +0500

    2nd

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAoj8YZkUCE9wZBDoEbYHGtqK7dj_4oudo",
-  authDomain: "role-based-dashboard.firebaseapp.com",
-  projectId: "role-based-dashboard",
-  storageBucket: "role-based-dashboard.appspot.com",
-  messagingSenderId: "1083774049699",
-  appId: "1:1083774049699:web:8d11c65e14201f39aa4e16",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
